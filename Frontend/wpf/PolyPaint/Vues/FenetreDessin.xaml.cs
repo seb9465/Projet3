d50b28da27865a2ef6d02797f027df9e86425fda
ollaua <olivier.lauzon@polymtl.ca>
2019-01-15 16:55:19 -0500

    save/load strokes

--- Frontend/wpf/PolyPaint/Vues/FenetreDessin.xaml.cs
+++ Frontend/wpf/PolyPaint/Vues/FenetreDessin.xaml.cs
@@ -7,6 +7,7 @@ using PolyPaint.VueModeles;
 using System.Windows.Media.Imaging;
 using System.IO;
 using Microsoft.Win32;
+using System.Windows.Ink;
 
 namespace PolyPaint
 {
@@ -49,40 +50,103 @@ namespace PolyPaint
 
         private void SaveImage(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.DefaultExt = ".png"; // Default file extension
-            saveFileDialog.Filter = "Image (.png)|*.png"; // Filter files by extension
+            // Save Image PNG (Probably gonna be used in the gallery later).
+            //{
+            //    SaveFileDialog saveFileDialog = new SaveFileDialog();
+            //    saveFileDialog.DefaultExt = ".png"; // Default file extension
+            //    saveFileDialog.Filter = "Image (.png)|*.png"; // Filter files by extension
+
+            //    // Show save file dialog box
+            //    Nullable<bool> result = saveFileDialog.ShowDialog();
+
+            //    // Get the dimensions of the ink control
+            //    var size = new Size(surfaceDessin.ActualWidth, surfaceDessin.ActualHeight);
+            //    surfaceDessin.Margin = new Thickness(0, 0, 0, 0);
+            //    surfaceDessin.Measure(size);
+            //    surfaceDessin.Arrange(new Rect(size));
+
+            //    int margin = (int)surfaceDessin.Margin.Left;
+            //    int width = (int)surfaceDessin.ActualWidth - margin;
+            //    int height = (int)surfaceDessin.ActualHeight - margin;
+
+            //    // Render ink to bitmap
+            //    RenderTargetBitmap rtb =
+            //    new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
+            //    rtb.Render(surfaceDessin);
+
+            //    // Save the ink to a memory stream
+            //    BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+            //    encoder.Frames.Add(BitmapFrame.Create(rtb));
+            //    byte[] bitmapBytes;
+            //    using (MemoryStream ms = new MemoryStream())
+            //    {
+            //        encoder.Save(ms);
+            //        // Get the bitmap bytes from the memory stream
+            //        ms.Position = 0;
+            //        bitmapBytes = ms.ToArray();
+            //    }
+
+            //    System.IO.File.WriteAllBytes(saveFileDialog.FileName, bitmapBytes);
+            //}
+
+            // Save Strokes to be able to reload it.
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.DefaultExt = ".stro"; // Default file extension
+                saveFileDialog.Filter = "Strokes (.stro)|*.stro"; // Filter files by extension
+
+                // Show save file dialog box
+                Nullable<bool> result = saveFileDialog.ShowDialog();
+
+                FileStream fs = null;
+
+                try
+                {
+                    fs = new FileStream(saveFileDialog.FileName, FileMode.Create);
+                    surfaceDessin.Strokes.Save(fs);
+                }
+                finally
+                {
+                    if (fs != null)
+                    {
+                        fs.Close();
+                    }
+                }
+
+            }
+        }
+
+        private void LoadImage(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = ".stro";
+            openFileDialog.Filter = "Strokes (.stro)|*.stro"; // Filter files by extension
 
             // Show save file dialog box
-            Nullable<bool> result = saveFileDialog.ShowDialog();
+            Nullable<bool> result = openFileDialog.ShowDialog();
 
-            // Get the dimensions of the ink control
-            var size = new Size(surfaceDessin.ActualWidth, surfaceDessin.ActualHeight);
-            surfaceDessin.Margin = new Thickness(0, 0, 0, 0);
-            surfaceDessin.Measure(size);
-            surfaceDessin.Arrange(new Rect(size));
+            FileStream fs = null;
 
-            int margin = (int)surfaceDessin.Margin.Left;
-            int width = (int)surfaceDessin.ActualWidth - margin;
-            int height = (int)surfaceDessin.ActualHeight - margin;
-            // Render ink to bitmap
-            RenderTargetBitmap rtb =
-            new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
-            rtb.Render(surfaceDessin);
-            // Save the ink to a memory stream
-            BmpBitmapEncoder encoder = new BmpBitmapEncoder();
-            encoder.Frames.Add(BitmapFrame.Create(rtb));
-            byte[] bitmapBytes;
-            using (MemoryStream ms = new MemoryStream())
+            if (!File.Exists(openFileDialog.FileName))
             {
-                encoder.Save(ms);
-                // Get the bitmap bytes from the memory stream
-                ms.Position = 0;
-                bitmapBytes = ms.ToArray();
+                MessageBox.Show("The file you requested does not exist." +
+                    " Save the StrokeCollection before loading it.");
+                return;
             }
-            System.IO.File.WriteAllBytes(saveFileDialog.FileName, bitmapBytes);
 
-
+            try
+            {
+                fs = new FileStream(openFileDialog.FileName, FileMode.Open, FileAccess.Read);
+                StrokeCollection strokes = new StrokeCollection(fs);
+                surfaceDessin.Strokes = strokes;
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
         }
     }
 }
