4b9bc8097069f6f8b67c4e571cb33436c8be9842
Alexis Loiselle <alexisloiselle97@hotmail.com>
2019-01-18 16:50:32 -0500

    link user to connection id (bug with dict)

--- Backend/PolyPaint.WebSocketAPI/Hubs/PolyPaintHub.cs
+++ Backend/PolyPaint.WebSocketAPI/Hubs/PolyPaintHub.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
 namespace PolyPaint.Hubs
 {
     public class PolyPaintHub : Hub
     {
+        private ConcurrentDictionary<string, string> userIds { get; set; }
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
@@ -17,8 +19,13 @@ namespace PolyPaint.Hubs
 
         public override async Task OnConnectedAsync()
         {
-            await base.OnConnectedAsync();
-            await Groups.AddToGroupAsync(Context.ConnectionId, "test");
+            string username = Context.GetHttpContext().Request.Query["user"];
+            if (username.Trim().Length != 0)
+            {
+                await base.OnConnectedAsync();
+                userIds.AddOrUpdate(Context.ConnectionId, username, (key, old) => username);
+                await Groups.AddToGroupAsync(Context.ConnectionId, "test");
+            }
         }
     }
 }
