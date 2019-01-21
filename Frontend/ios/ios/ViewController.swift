82aa103c1b9a12b1085cf9fac629452231d8aefb
Sebastien Cadorette <cado.94@me.com>
2019-01-21 15:40:37 -0500

    link storyboard objects to the code + email validation on login click

--- Frontend/ios/ios/ViewController.swift
+++ Frontend/ios/ios/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var emailField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var validationLabel: UILabel!
+    
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    @IBAction func loginButton(_ sender: Any) {
+        let valid = isValidEmail(testStr: emailField.text!)
+        
+        if valid {
+            validationLabel.text = "Valid"
+        } else {
+            validationLabel.text = "Invalid"
+        }
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
