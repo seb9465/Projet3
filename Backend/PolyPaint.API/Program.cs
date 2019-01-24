1a834dca51c5802edb5547ddec312b662178311b
ollaua <olivier.lauzon@polymtl.ca>
2019-01-24 10:58:15 -0500

    Adapt new buttons to materialDesign... localDb usage for now.

--- Backend/PolyPaint.API/Program.cs
+++ Backend/PolyPaint.API/Program.cs
@@ -32,7 +32,7 @@ namespace PolyPaint.API
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>().UseIISIntegration();
+                .UseStartup<Startup>().UseUrls("http://*:4000");
         }
     }
 }
