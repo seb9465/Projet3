325a7b85cea45635d9c05bf14b72f0f0c0193dc7
Sébastien Labine <sebastien.labine@polymtl.ca>
2019-01-24 23:14:16 -0500

    Premier jet pour le UI

---
+++ Frontend/wpf/PolyPaint/Vues/Login.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace PolyPaint.Vues
+{
+    /// <summary>
+    /// Logique d'interaction pour Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        private Register register;
+        public Login()
+        {
+            InitializeComponent();
+            register = new Register();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameBox.Text;
+            string password = passwordBox.Password;
+            this.Content = register;
+        }
+
+        private void OfflineBtn_Click(object sender, RoutedEventArgs e)
+        {
+            /*          FenetreDessin main = new FenetreDessin();
+                      App.Current.MainWindow = main;
+                      this.Close();
+                      main.Show();*/
+        }
+    }
+}
---
+++ Frontend/wpf/PolyPaint/Vues/Register.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PolyPaint.Vues
+{
+    public partial class Register : Page
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Content = new Login();
+        }
+
+        private void RegisterBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
