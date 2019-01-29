f3c4a1ebf53b331effabe00739f421dfff2efbda
Sebastien Cadorette <cado.94@me.com>
2019-01-29 14:52:10 -0500

    Ajout du pod SwiftSignalRClient pour le chat sur client léger.

--- Frontend/ios/ios/ChatController.swift
+++ Frontend/ios/ios/ChatController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import SwiftSignalRClient
 
 class ChatController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var label: UILabel!
+    
+    let chatURL = "http://10.200.19.14:5000/signalr"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //let query: [String: String] = ["parameterA": "valueA", "parameterB": "valueB"]
+        let hubConnection = HubConnectionBuilder(url: URL(string: chatURL)!)
+            .build()
+        hubConnection.start()
+        
+        hubConnection.invoke(method: "ConnectToGroup", arguments: ["abcde"], invocationDidComplete: { error in
+            if let e = error {
+                self.label.text = "connected"
+            }
+        })
+        
+        
     }
 }
