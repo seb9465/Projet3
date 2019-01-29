b9765a8bdc303afb02b7330678eda0a2e1c53eea
Sebastien Cadorette <cado.94@me.com>
2019-01-29 16:37:33 -0500

    Connexion & envoie de messages fonctionnels avec l'utilisation du token du login.

--- Frontend/ios/ios/ChatController.swift
+++ Frontend/ios/ios/ChatController.swift
@@ -13,6 +13,7 @@ class ChatController: UIViewController, UITextFieldDelegate {
     @IBOutlet var statusLabel: UILabel!
     @IBOutlet var receivedMessage: UILabel!
     @IBOutlet var connectButton: UIButton!
+    @IBOutlet var sendMessageButton: UIButton!
     
     let chatURL = "http://10.200.19.14:5000/signalr"
     var hubConnection: HubConnection!
@@ -24,6 +25,7 @@ class ChatController: UIViewController, UITextFieldDelegate {
                 self.statusLabel.text = "Connected";
                 self.statusLabel.textColor = UIColor.green;
                 self.connectButton.isEnabled = false;
+                self.sendMessageButton.isEnabled = true;
             } else {
                 print("error");
                 print(error as Any);
@@ -33,17 +35,27 @@ class ChatController: UIViewController, UITextFieldDelegate {
         })
     }
     
+    @IBAction func sendMessageTrigger(_ sender: Any) {
+            self.hubConnection.invoke(method: "SendMessage", arguments: ["hello"], invocationDidComplete: { args in
+                    print("Message sent")
+                })
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.sendMessageButton.isEnabled = false;
+        
+        // Token avec /api/token dans postman avec un user et un password en param
         self.hubConnection = HubConnectionBuilder(url: URL(string: chatURL)!)
+            .withHttpConnectionOptions() { httpConnectionOptions in
+                httpConnectionOptions.accessTokenProvider = { return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InNlYmFzIiwibmFtZWlkIjoiMTc4ZDAyMTYtZjMzYS00OWE1LWIxZWYtNWY1NDVhMGE2NTkzIiwibmJmIjoxNTQ4Nzk3NzEyLCJleHAiOjYxNTQ4Nzk3NjUyLCJpYXQiOjE1NDg3OTc3MTIsImlzcyI6IjEwLjIwMC4yNy4xNjo1MDAxIiwiYXVkIjoiMTAuMjAwLjI3LjE2OjUwMDEifQ.Am6W-nUbklrC4cV-w2NxhI56Df9awzFdXhtwGoihqDU" }}
             .build()
         
         self.hubConnection.start()
         
-//        self.hubConnection.invoke(method: "SendMessage", arguments: ["hello"], invocationDidComplete: { args in
-//            print("Message sent")
-//            })
+
         
         self.hubConnection.on(method: "ReceiveMessage", callback: { args, typeConverter in
             print("Message Sent")
