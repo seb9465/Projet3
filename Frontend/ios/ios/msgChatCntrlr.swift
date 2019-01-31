f3cf6db58b3f0db6281d60189905f97c28bfc9dd
Sebastien Cadorette <cado.94@me.com>
2019-01-31 18:57:07 -0500

    Ajout d'un contrôleur pour la nouvelle vue du chat.

---
+++ Frontend/ios/ios/msgChatCntrlr.swift
@@ -0,0 +1,14 @@
+//
+//  msgChatCntrlr.swift
+//  ios
+//
+//  Created by Sebastien Cadorette on 2019-01-31.
+//  Copyright © 2019 LOG3000 equipe 12. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+
+class MsgChatController: MessagesViewController {
+    
+}
