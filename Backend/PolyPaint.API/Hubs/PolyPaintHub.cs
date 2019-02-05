12f12c0266bd9fbe5313fbd1b11f191de50e8604
Alexis Loiselle <alexisloiselle97@hotmail.com>
2019-02-05 14:03:30 -0500

    revert send all but sender

--- Backend/PolyPaint.API/Hubs/PolyPaintHub.cs
+++ Backend/PolyPaint.API/Hubs/PolyPaintHub.cs
@@ -29,8 +29,7 @@ namespace PolyPaint.API.Hubs
                 if (user != null && UserHandler.UserGroupMap.TryGetValue(userId, out var groupId))
                 {
                     // Maybe change in a way that it doesnt send back to sender (sender handles his own message in the ui)
-                    await Clients.GroupExcept(groupId, Context.ConnectionId)
-                        .SendAsync("ReceiveMessage", user.UserName, message, timestamp);
+                    await Clients.Group(groupId).SendAsync("ReceiveMessage", user.UserName, message, timestamp);
                 }
             }
         }
