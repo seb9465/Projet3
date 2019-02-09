3bf4bd994851567376f78d0ca7550892ae9e8ba2
Sebastien Cadorette <cado.94@me.com>
2019-02-09 14:10:15 -0500

    Ajout d'un fichier de Service pour le clavardage.

---
+++ Frontend/ios/ios/Clavardage/ChatService.swift
@@ -0,0 +1,100 @@
+//
+//  ChatService.swift
+//  ios
+//
+//  Created by Sebastien Cadorette on 2019-02-09.
+//  Copyright © 2019 LOG3000 equipe 12. All rights reserved.
+//
+
+import Foundation
+import SwiftSignalRClient
+import PromiseKit
+
+class ChatService {
+    var hubConnection: HubConnection;
+    
+    init() {
+        self.hubConnection = HubConnectionBuilder(url: URL(string: Constants.CHAT_URL)!)
+            .withHttpConnectionOptions() { httpConnectionOptions in
+                httpConnectionOptions.accessTokenProvider = { return USER_TOKEN; }}
+            .build();
+    }
+    
+    public func connectToHub() -> Void {
+        self.hubConnection.start();
+    }
+    
+    public func initOnReceivingMessage(currentMemberName: String, function: @escaping (_ message: Message) -> Void) {
+        self.hubConnection.on(method: "ReceiveMessage", callback: { args, typeConverter in
+            let user = try! typeConverter.convertFromWireType(obj: args[0], targetType: String.self);
+            let message = try! typeConverter.convertFromWireType(obj: args[1], targetType: String.self);
+            let timestamp = try! typeConverter.convertFromWireType(obj: args[2], targetType: String.self);
+            
+            let newMember = Member(
+                name: user!,
+                color: .random
+            );
+            
+            let newMessage = Message(
+                member: newMember,
+                text: message!,
+                timestamp: timestamp!,
+                messageId: UUID().uuidString
+            );
+            
+            if (user != currentMemberName) {
+                function(newMessage);
+            }
+        });
+    }
+    
+    public func initOnAnotherUserConnection(function: @escaping (_ message: Message) -> Void) -> Void {
+        self.hubConnection.on(method: "SystemMessage", callback: { args, typeConverter in
+            let message = try! typeConverter.convertFromWireType(obj: args[0], targetType: String.self);
+            
+            let sysMember = Member(
+                name: "SYSTEM",
+                color: .random
+            );
+            
+            let newMessage = Message(
+                member: sysMember,
+                text: message!,
+                timestamp: Constants.formatter.string(from: Date()),
+                messageId: UUID().uuidString
+            );
+            
+            function(newMessage);
+        });
+    }
+    
+    public func connectToGroup() -> Void {
+        self.hubConnection.on(method: "ClientIsConnected", callback: { args, typeConverter in
+            self.hubConnection.invoke(method: "ConnectToGroup", arguments: [""], invocationDidComplete: { error in
+                if (error != nil) {
+                    print("Error connecting to server!")
+                    print(error as Any);
+                }
+                print("Connected to the group");
+            });
+        });
+    }
+    
+    public func disconnectFromHub() -> Void {
+        self.hubConnection.stop();
+    }
+    
+    public func sendMessage(message: Message, function: @escaping (_ message: Message) -> Void) -> Promise<Any> {
+        return Promise{seal in
+            self.hubConnection.invoke(method: "SendMessage", arguments: [message.text], invocationDidComplete: { error in
+                if let e = error {
+                    print("ERROR");
+                    print(e);
+                }
+                
+                // Insert message
+                function(message);
+            })
+        }
+    }
+}
