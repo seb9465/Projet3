c4b0c4ee016db845648c6db26fe6a2c90cb8c148
Sebastien Cadorette <cado.94@me.com>
2019-02-09 14:56:26 -0500

    Création d'une classe Membres qui contiendra la liste des membres connectés à un groupe de clavardage.

--- Frontend/ios/ios/Chat/Members.swift
+++ Frontend/ios/ios/Chat/Members.swift
@@ -9,6 +9,48 @@
 import Foundation
 import UIKit
 
+class Members {
+    private var _members: [Member];
+    
+    var members: [Member] {
+        get { return _members }
+    }
+    
+    init() {
+        self._members = [];
+    }
+    
+    public func addMember(member: Member) -> Bool {
+        if (!self.isAlreadyInArray(member: member)) {
+            self._members.append(member);
+            
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public func removeFromArray(member: Member) -> Bool {
+        if (self.isAlreadyInArray(member: member)) {
+            self._members.remove(at: self.getIndexOfMember(member: member));
+            
+            return true;
+        }
+        
+        return false;
+    }
+    
+    private func isAlreadyInArray(member: Member) -> Bool {
+        return self._members.contains(where: { $0.name == member.name });
+    }
+    
+    private func getIndexOfMember(member: Member) -> Int {
+        return self._members.index(where: { (memb) -> Bool in
+            memb.name == member.name
+        }) ?? -1;
+    }
+}
+
 class Member {
     private let _name: String;
     private let _color: UIColor;
