73923c9f179d77e67146eef3bd9160fc9c0c4fb2
Sebastien Cadorette <cado.94@me.com>
2019-02-09 14:04:28 -0500

    Ajout d'un fichier de constantes.

---
+++ Frontend/ios/ios/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  ios
+//
+//  Created by Sebastien Cadorette on 2019-02-09.
+//  Copyright © 2019 LOG3000 equipe 12. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let SERVER_BASE_URL: String = "https://polypaint.me";
+    static let CHAT_URL: String = Constants.SERVER_BASE_URL + "/signalr";
+    static let formatter: DateFormatter = {
+        let formatter = DateFormatter();
+        formatter.dateFormat = "HH:mm:ss";
+        return formatter;
+    }()
+    
+}
