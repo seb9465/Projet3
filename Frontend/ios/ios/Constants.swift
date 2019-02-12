88d26335463e7def6dfda096593470068ed4b62e
Sebastien Cadorette <cado.94@me.com>
2019-02-12 15:09:23 -0500

    Ajustement de l'adresse pour le serveur distant.

--- Frontend/ios/ios/Constants.swift
+++ Frontend/ios/ios/Constants.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct Constants {
-    static let SERVER_BASE_URL: String = "http://10.200.19.251:5000";
+    static let SERVER_BASE_URL: String = "https://polypaint.me";
     static let CHAT_URL: String = Constants.SERVER_BASE_URL + "/signalr";
     static let LOGIN_URL: String = Constants.SERVER_BASE_URL + "/api/login";
     static let REGISTER_URL: String = Constants.SERVER_BASE_URL + "/api/register";
