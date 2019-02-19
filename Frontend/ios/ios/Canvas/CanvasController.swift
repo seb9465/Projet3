96598719097d48b8f13ec684e3bf7345887e2aeb
Sebastien Cadorette <cado.94@me.com>
2019-02-19 13:27:55 -0500

    Ajout d'un service pour le Canvas. Ajout d'un bouton pour quitter le Canvas.

--- Frontend/ios/ios/Canvas/CanvasController.swift
+++ Frontend/ios/ios/Canvas/CanvasController.swift
@@ -11,6 +11,32 @@ import UIKit
 class CanvasController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
-        print("View Did Appear");
+        super.viewDidAppear(animated);
+        
+        let width: CGFloat = 240.0
+        let height: CGFloat = 160.0
+        
+        let demoView = DemoView(frame: CGRect(x: self.view.frame.size.width/2 - width/2,
+                                              y: self.view.frame.size.height/2 - height/2,
+                                              width: width,
+                                              height: height))
+        
+        self.view.addSubview(demoView)
+    }
+    
+    @IBAction func quitButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Alert", message: "Would you like to quit ?", preferredStyle: .alert)
+        
+        let yesAction: UIAlertAction = UIAlertAction(title: "Yes", style: .default) { _ in
+            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil);
+            let viewController: UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "DashboardController");
+            self.present(viewController, animated: true, completion: nil);
+        }
+        let noAction: UIAlertAction = UIAlertAction(title: "No", style: .default, handler: nil);
+        
+        alert.addAction(yesAction);
+        alert.addAction(noAction);
+        
+        self.present(alert, animated: true, completion: nil)
     }
 }
