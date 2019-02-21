d79f0e3f82e4755e7ae671b6977aedc0ce4f26b0
Sebastien Cadorette <cado.94@me.com>
2019-02-21 15:22:12 -0500

    Ajout d'un fichier qui contiendra le controle d'une figure.

---
+++ Frontend/ios/ios/Canvas/figureService.swift
@@ -0,0 +1,21 @@
+//
+//  figureService.swift
+//  ios
+//
+//  Created by Sebastien Cadorette on 2019-02-21.
+//  Copyright © 2019 LOG3000 equipe 12. All rights reserved.
+//
+
+import UIKit
+
+class figureService: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
