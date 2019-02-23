acc0623fa734220ec5286da42187dd9ab18f2d67
Sebastien Cadorette <cado.94@me.com>
2019-02-23 12:15:40 -0500

    creds

--- Frontend/ios/ios/Authentification/LoginController.swift
+++ Frontend/ios/ios/Authentification/LoginController.swift
@@ -25,14 +25,14 @@ class LoginController: UIViewController, UITextFieldDelegate {
         
         let validEmail: Bool = isValidEmail(testStr: emailField.text!);
         
-        let parameters = [
-            "username": emailField.text,
-            "password": passwordField.text
-        ]
 //        let parameters = [
-//            "username": "seb.cado",
-//            "password": "!12345Aa"
+//            "username": emailField.text,
+//            "password": passwordField.text
 //        ]
+        let parameters = [
+            "username": "seb.cado",
+            "password": "!12345Aa"
+        ]
         
         self.authenticateUser(parameters: parameters).done { response in
             UIViewController.removeSpinner(spinner: sv);
