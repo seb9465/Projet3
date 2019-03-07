c2d51840dd1c8ec703bae312408be1bb1ea051c4
Sebastien Cadorette <cado.94@me.com>
2019-03-07 11:05:02 -0500

    Suppression du code mis en commentaire lors du refactor.

--- Frontend/ios/ios/Canvas/FigureService.swift
+++ Frontend/ios/ios/Canvas/FigureService.swift
@@ -28,30 +28,24 @@ class FigureService: UIView {
     private var currentPoint: CGPoint?;
     private var previousPoint1: CGPoint?;
     
-//    private let radius: CGFloat = 5.0;
-//    private var selectedDashedBorder: CAShapeLayer!;
-//    private var selectedCornerCircle1: CAShapeLayer!;
-//    private var selectedCornerCircle2: CAShapeLayer!;
-//    private var selectedCornerCircle3: CAShapeLayer!;
-//    private var selectedCornerCircle4: CAShapeLayer!;
-    
     private var selectFigureService: SelectFigureService;
     private var currentTool: FigureProtocol
     
     // MARK: Public functions
     
     init(origin: CGPoint) {
-        self.currentTool = Rect();
-        self.currentTool.setInitialPoint(xPoint: origin.x - 50, yPoint: origin.y - 50);
-        self.currentTool.setLastPoint(xPoint: origin.x + 50, yPoint: origin.y + 50);
         let fPoint: CGPoint = CGPoint(x: origin.x - 50, y: origin.y - 50);
         let lPoint: CGPoint = CGPoint(x: origin.x + 50, y: origin.y + 50);
+        
+        self.currentTool = Rect();
+        self.currentTool.setInitialPoint(initialPoint: fPoint);
+        self.currentTool.setLastPoint(lastPoint: lPoint);
+        
         self.selectFigureService = SelectFigureService();
         
         super.init(frame: CGRect(x: origin.x - 50, y: origin.y - 50, width: 103, height: 103));
         
         self.selectFigureService.setInitialSelectedDashedBorder(bounds: self.bounds);
-        
         self.selectFigureService.setInitialSelectedCornerCirles(firstPoint: fPoint, lastPoint: lPoint);
         
         self.backgroundColor = UIColor.clear;
@@ -168,63 +162,4 @@ class FigureService: UIView {
         self.currentTool.lineColor = borderColor;
         setNeedsDisplay();
     }
-    
-    // MARK: Private functions
-    
-//    private func setInitialSelectedCornerCirles() -> Void {
-//        selectedCornerCircle1 = CAShapeLayer();
-//        selectedCornerCircle1.path = UIBezierPath(roundedRect: CGRect(x: -5, y: -5, width: 2.0 * self.radius, height: 2.0 * self.radius), cornerRadius: self.radius).cgPath;
-//        selectedCornerCircle1.position = CGPoint(x: 0, y: 0);
-//        selectedCornerCircle1.fillColor = UIColor.blue.cgColor;
-//
-//        selectedCornerCircle2 = CAShapeLayer();
-//        selectedCornerCircle2.path = UIBezierPath(roundedRect: CGRect(x: -5, y: -5, width: 2.0 * self.radius, height: 2.0 * self.radius), cornerRadius: self.radius).cgPath;
-//        selectedCornerCircle2.position = CGPoint(x: self.currentTool.lastPoint.x - self.currentTool.firstPoint.x + 2, y: 0);
-//        selectedCornerCircle2.fillColor = UIColor.blue.cgColor;
-//
-//        selectedCornerCircle3 = CAShapeLayer();
-//        selectedCornerCircle3.path = UIBezierPath(roundedRect: CGRect(x: -5, y: -5, width: 2.0 * self.radius, height: 2.0 * self.radius), cornerRadius: self.radius).cgPath;
-//        selectedCornerCircle3.position = CGPoint(x: self.currentTool.lastPoint.x - self.currentTool.firstPoint.x + 2, y: self.currentTool.lastPoint.y - self.currentTool.firstPoint.y + 2);
-//        selectedCornerCircle3.fillColor = UIColor.blue.cgColor;
-//
-//        selectedCornerCircle4 = CAShapeLayer();
-//        selectedCornerCircle4.path = UIBezierPath(roundedRect: CGRect(x: -5, y: -5, width: 2.0 * self.radius, height: 2.0 * self.radius), cornerRadius: self.radius).cgPath;
-//        selectedCornerCircle4.position = CGPoint(x: 0, y: self.currentTool.lastPoint.y - self.currentTool.firstPoint.y + 2);
-//        selectedCornerCircle4.fillColor = UIColor.blue.cgColor;
-//    }
-    
-//    private func setInitialSelectedDashedBorder() -> Void {
-//        selectedDashedBorder = CAShapeLayer();
-//        selectedDashedBorder.strokeColor = UIColor.black.cgColor;
-//        selectedDashedBorder.lineDashPattern = [4, 4];
-//        selectedDashedBorder.frame = self.bounds;
-//        selectedDashedBorder.fillColor = nil;
-//        selectedDashedBorder.path = UIBezierPath(rect: self.bounds).cgPath;
-//    }
-//    
-//    private func addSelectedFigureLayers() -> Void {
-//        self.layer.addSublayer(selectedDashedBorder);
-//        self.layer.addSublayer(selectedCornerCircle1);
-//        self.layer.addSublayer(selectedCornerCircle2);
-//        self.layer.addSublayer(selectedCornerCircle3);
-//        self.layer.addSublayer(selectedCornerCircle4);
-//    }
-//    
-//    private func removeSelectedFigureLayers() -> Void {
-//        self.selectedDashedBorder.removeFromSuperlayer();
-//        self.selectedCornerCircle1.removeFromSuperlayer();
-//        self.selectedCornerCircle2.removeFromSuperlayer();
-//        self.selectedCornerCircle3.removeFromSuperlayer();
-//        self.selectedCornerCircle4.removeFromSuperlayer();
-//    }
-//    
-//    private func adjustSelectedFigureLayers() -> Void {
-//        selectedDashedBorder.path = UIBezierPath(rect: self.bounds).cgPath;
-//        selectedCornerCircle2.position.x = self.currentTool.lastPoint.x - self.currentTool.firstPoint.x + 2;
-//        selectedCornerCircle3.position.x = self.currentTool.lastPoint.x - self.currentTool.firstPoint.x + 2;
-//        selectedCornerCircle3.position.y = self.currentTool.lastPoint.y - self.currentTool.firstPoint.y + 2;
-//        selectedCornerCircle4.position.y = self.currentTool.lastPoint.y - self.currentTool.firstPoint.y + 2;
-//        self.addSelectedFigureLayers();
-//        setNeedsDisplay();
-//    }
 }
