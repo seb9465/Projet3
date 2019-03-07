fb85027d2348cb1cb46481482e277fe52a368143
Sebastien Cadorette <cado.94@me.com>
2019-03-07 16:14:58 -0500

    Suppression de code commenté inutilié.

--- Frontend/ios/ios/Chat/CustomCell/MyCustomMessagesFlowLayout.swift
+++ Frontend/ios/ios/Chat/CustomCell/MyCustomMessagesFlowLayout.swift
@@ -26,12 +26,4 @@ open class CustomMessageSizeCalculator: MessageSizeCalculator {
         super.init()
         self.layout = layout
     }
-    
-//    open override func sizeForItem(at indexPath: IndexPath) -> CGSize {
-//            guard let layout = layout else { return .zero }
-//            let collectionViewWidth = layout.collectionView?.bounds.width ?? 0
-//            let contentInset = layout.collectionView?.contentInset ?? .zero
-//            let inset = layout.sectionInset.left + layout.sectionInset.right + contentInset.left + contentInset.right
-//            return CGSize(width: collectionViewWidth - inset, height: 44)
-//    }
 }
