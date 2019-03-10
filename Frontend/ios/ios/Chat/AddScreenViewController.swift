895f2481bb50a01ee161584af0d32fada6f29c70
Sebastien Cadorette <cado.94@me.com>
2019-03-09 19:15:39 -0500

    Nouvelle classe pour ajouter un nouveau groupe de discussion.

---
+++ Frontend/ios/ios/Chat/AddScreenViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddScreenViewController.swift
+//  ios
+//
+//  Created by Sebastien Cadorette on 2019-03-09.
+//  Copyright © 2019 LOG3000 equipe 12. All rights reserved.
+//
+
+import Foundation
