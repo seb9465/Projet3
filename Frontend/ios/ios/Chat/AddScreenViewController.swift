65c7ae67d7b09107ccabc1fb5b4ec3cd69909ed0
Sebastien Cadorette <cado.94@me.com>
2019-03-09 19:36:55 -0500

    Ébauche de classe AddScreenViewController.

--- Frontend/ios/ios/Chat/AddScreenViewController.swift
+++ Frontend/ios/ios/Chat/AddScreenViewController.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+import UIKit
+
+class AddScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+    
+}
