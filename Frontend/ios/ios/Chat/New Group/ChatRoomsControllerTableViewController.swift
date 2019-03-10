b601e28552843adea59c4d8a16b858f8d9368c43
Sebastien Cadorette <cado.94@me.com>
2019-03-10 12:16:12 -0400

    Ajout d'un nouveau fichier pour un custom Header.

--- Frontend/ios/ios/Chat/New Group/ChatRoomsControllerTableViewController.swift
+++ Frontend/ios/ios/Chat/New Group/ChatRoomsControllerTableViewController.swift
@@ -62,21 +62,19 @@ class ChatRoomsControllerTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-//        return self.channels.channels.count;
         return 1;
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 1;
         return self.channels.channels.count;
     }
-
-//    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-//        return 15;
-//    }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100;
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let storyboard = UIStoryboard(name: "ChatRoomTitle", bundle: nil);
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
