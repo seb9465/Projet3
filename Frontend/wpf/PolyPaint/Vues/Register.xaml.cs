5e45b36f53a46b394f850ac0ef574779bc99c9f9
chbinc <chelsy.binet@polymtl.ca>
2019-03-11 14:49:32 -0400

    verification register

--- Frontend/wpf/PolyPaint/Vues/Register.xaml.cs
+++ Frontend/wpf/PolyPaint/Vues/Register.xaml.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.AspNetCore.Http;
+using PolyPaint.VueModeles;
 
 namespace PolyPaint.Vues
 {
@@ -23,6 +29,33 @@ namespace PolyPaint.Vues
         private void Register_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void IsEmailValid(object sender, RoutedEventArgs e)
+        {
+            string input = (sender as TextBox).Text;
+
+            if (!Regex.IsMatch(input, @"^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$"))
+            {
+                InvalidEmail.Opacity = 100;
+            } else
+            {
+                InvalidEmail.Opacity = 0;
+            }
+        }
+
+        private void IsPasswordValid(object sender, RoutedEventArgs e)
+        {
+            string input = (sender as PasswordBox).Password;
+
+            if (!Regex.IsMatch(input, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$"))
+            {
+                InvalidPassword.Opacity = 100;
+            }
+            else
+            {
+                InvalidPassword.Opacity = 0;
+            }
         }
     }
 }
