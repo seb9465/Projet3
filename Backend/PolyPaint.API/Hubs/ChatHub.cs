faa0e79cb564b0238e73c965f1835b5e19822888
chbinc <chelsy.binet@polymtl.ca>
2019-03-12 10:00:46 -0400

    rooms work

--- Backend/PolyPaint.API/Hubs/ChatHub.cs
+++ Backend/PolyPaint.API/Hubs/ChatHub.cs
@@ -10,11 +10,13 @@ using System.Threading.Tasks;
 
 namespace PolyPaint.API.Hubs
 {
-    public class ChatHub : BaseHub
+    public class ChatHub : Hub
     {
+        private readonly UserService _userService;
 
-        public ChatHub(UserService userService) : base(userService)
+        public ChatHub(UserService userService)
         {
+            _userService = userService;
         }
 
         public async Task SendMessage(ChatMessage chatMessage)
@@ -31,6 +33,122 @@ namespace PolyPaint.API.Hubs
                         new ChatMessage(user.UserName, chatMessage.Message, chatMessage.ChannelId, timestamp)
                     );
                 }
+            }
+        }
+
+        public async Task FetchChannels()
+        {
+            var user = await GetUserFromToken(Context.User);
+            if (user != null)
+            {
+                List<Channel> list = UserHandler.UserGroupMap.Select(x => new Channel(x.Key, x.Value.Contains(user.Id))).ToList();
+                ChannelsMessage channelsMessage = new ChannelsMessage(list);
+                await Clients.Caller.SendAsync("FetchChannels", channelsMessage);
+            }
+        }
+
+        public async Task CreateChannel(ChannelMessage channelMessage)
+        {
+
+            var user = await GetUserFromToken(Context.User);
+            if (user != null)
+            {
+                UserHandler.AddOrUpdateMap(channelMessage.Channel.Name, user.Id);
+                await Clients.Caller.SendAsync("CreateChannel", channelMessage);
+            }
+        }
+
+        public async Task ConnectToChannel(ConnectionMessage connectionMessage)
+        {
+            var user = await GetUserFromToken(Context.User);
+            if (user != null)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, connectionMessage.ChannelId);
+                UserHandler.AddOrUpdateMap(connectionMessage.ChannelId, user.Id);
+                var message = new ConnectionMessage(user.UserName, channelId: connectionMessage.ChannelId);
+                await Clients.Group(connectionMessage.ChannelId).SendAsync(
+                    "ConnectToChannel",
+                    message
+                );
+                await Clients.Caller.SendAsync("ConnectToChannelSender", message);
+            }
+        }
+
+        public async Task DisconnectFromChannel(ConnectionMessage connectionMessage)
+        {
+            var user = await GetUserFromToken(Context.User);
+            if (user != null)
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, connectionMessage.ChannelId);
+                if (UserHandler.UserGroupMap.TryGetValue(connectionMessage.ChannelId, out var list))
+                {
+                    list.Remove(user.Id);
+                    var message = new ConnectionMessage(username: user.UserName, channelId: connectionMessage.ChannelId);
+                    await Clients.Group(connectionMessage.ChannelId).SendAsync(
+                        "DisconnectFromChannel",
+                        message
+                    );
+                    await Clients.Caller.SendAsync("DisconnectFromChannelSender", message);
+                }
+            }
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var user = await GetUserFromToken(Context.User);
+            if (user != null)
+            {
+                await base.OnConnectedAsync();
+                await ConnectToChannel(new ConnectionMessage(channelId: "general"));
+                await Clients.Caller.SendAsync("ClientIsConnected", "You are connected!");
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            if (_userService.TryGetUserId(Context.User, out var userId))
+            {
+                var user = await _userService.FindByIdAsync(userId);
+                await base.OnDisconnectedAsync(e);
+
+                foreach (var pair in UserHandler.UserGroupMap.Where(x => x.Value.Contains(user.Id)))
+                {
+                    if (user != null)
+                    {
+                        pair.Value.Remove(user.Id);
+                        await Clients.Group(pair.Key).SendAsync(
+                            "DisconnectFromChannel",
+                            new ConnectionMessage(username: user.UserName)
+                        );
+                    }
+                }
+            }
+        }
+
+        private async Task<ApplicationUser> GetUserFromToken(ClaimsPrincipal contextUser)
+        {
+            string userId;
+            if (_userService.TryGetUserId(contextUser, out userId))
+            {
+                var user = await _userService.FindByIdAsync(userId);
+                if (user != null)
+                {
+                    return user;
+                }
+                else
+                {
+                    await Clients.Caller.SendAsync("ErrorMessage",
+                        new ErrorMessage("Error occured while looking for user")
+                    );
+                    return null;
+                }
+            }
+            else
+            {
+                await Clients.Caller.SendAsync("ErrorMessage",
+                    new ErrorMessage("Error occured while looking for token")
+                );
+                return null;
             }
         }
     }
