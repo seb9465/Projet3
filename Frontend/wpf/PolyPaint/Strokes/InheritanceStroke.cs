74ba3f8a7a58c1d076484d90ef8fbd345b03969b
ollaua <olivier.lauzon@polymtl.ca>
2019-03-19 13:00:53 -0400

    Inheritance stroke done

--- Frontend/wpf/PolyPaint/Strokes/InheritanceStroke.cs
+++ Frontend/wpf/PolyPaint/Strokes/InheritanceStroke.cs
@@ -36,20 +36,36 @@ namespace PolyPaint.Strokes
             StylusPoint stp = StylusPoints[0];
             StylusPoint sp = StylusPoints[1];
 
-            drawingContext.DrawLine(pen, new Point(stp.X, stp.Y), new Point(sp.X - 10, sp.Y));
-            
-            Point point2 = new Point(sp.X - 10, sp.Y + 10);
-            Point point3 = new Point(sp.X - 10, sp.Y - 10);
+            int arrowLength = 10;
+            double dx = sp.X - stp.X;
+            double dy = sp.Y - stp.Y;
+
+            double theta = Math.Atan2(dy, dx);
+
+            double rad = (Math.PI / 180) * 35;
+            double x = sp.X - arrowLength * Math.Cos(theta + rad);
+            double y = sp.Y - arrowLength * Math.Sin(theta + rad);
+
+            double phi2 = (Math.PI / 180) * -35;
+            double x2 = sp.X - arrowLength * Math.Cos(theta + phi2);
+            double y2 = sp.Y - arrowLength * Math.Sin(theta + phi2);
+
+            Point point2 = new Point((x2 + x) / 2,(y2 + y) / 2);
+            Point point3 = new Point(x, y);
             Point point4 = new Point(sp.X, sp.Y);
+            Point point5 = new Point(x2, y2);
+            Point point6 = new Point((x2 + x) / 2, (y2 + y) / 2);
             StreamGeometry streamGeometry = new StreamGeometry();
             using (StreamGeometryContext geometryContext = streamGeometry.Open())
             {
-                geometryContext.BeginFigure(sp.ToPoint(), true, true);
+                geometryContext.BeginFigure(stp.ToPoint(), true, true);
                 PointCollection points = new PointCollection
                                              {
                                                  point2,
                                                  point3,
-                                                 point4
+                                                 point4,
+                                                 point5,
+                                                 point6
                                              };
                 geometryContext.PolyLineTo(points, true, true);
             }
