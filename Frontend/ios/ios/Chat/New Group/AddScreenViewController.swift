fe147728bb5b67d97140d3d325d6be4b7f83603e
Sebastien Cadorette <sebastien.cadorette@polymtl.ca>
2019-03-19 10:12:48 -0400

    Ajustement hauteur & nombre de cellules & sections.

--- Frontend/ios/ios/Chat/New Group/AddScreenViewController.swift
+++ Frontend/ios/ios/Chat/New Group/AddScreenViewController.swift
@@ -11,8 +11,16 @@ import UIKit
 
 class AddScreenViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60;
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 100;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
