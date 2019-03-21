d876254950293bffedf0a804ba0ded00a7df7646
William Sevigny <wsvgny@gmail.com>
2019-03-21 15:38:03 -0400

    Updated gallery

--- Frontend/ios/ios/Gallery/Cells/ContextMenuCell.swift
+++ Frontend/ios/ios/Gallery/Cells/ContextMenuCell.swift
@@ -1,19 +1,19 @@
+////
+////  ContextMenuCell.swift
+////  ios
+////
+////  Created by William Sevigny on 2019-02-22.
+////  Copyright © 2019 LOG3000 equipe 12. All rights reserved.
+////
 //
-//  ContextMenuCell.swift
-//  ios
+//import UIKit
 //
-//  Created by William Sevigny on 2019-02-22.
-//  Copyright © 2019 LOG3000 equipe 12. All rights reserved.
-//
-
-import UIKit
-
-class ContextMenuCell: UICollectionViewCell {
-    @IBOutlet weak var CanvasName: UILabel!
-    @IBOutlet weak var CanvasID: UILabel!
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        self.layer.cornerRadius = 5;
-    }
-}
+//class ContextMenuCell: UICollectionViewCell {
+//    @IBOutlet weak var CanvasName: UILabel!
+//    @IBOutlet weak var CanvasID: UILabel!
+//    
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//        self.layer.cornerRadius = 5;
+//    }
+//}
