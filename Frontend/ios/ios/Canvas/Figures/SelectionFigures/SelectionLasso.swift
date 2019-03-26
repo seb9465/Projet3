e319272bfa83827646a327cdfb4e44e4941dc091
Sebastien Cadorette <cado.94@me.com>
2019-03-26 14:13:39 -0400

    Ajout de point lors du clic pour la sélection & shape fermée en tous temps.

--- Frontend/ios/ios/Canvas/Figures/SelectionFigures/SelectionLasso.swift
+++ Frontend/ios/ios/Canvas/Figures/SelectionFigures/SelectionLasso.swift
@@ -14,12 +14,17 @@ class SelectionLasso: UIView {
     
     private var firstPoint: CGPoint;
     private var lastPoint: CGPoint;
+    private var points: [CGPoint];
     private var shape: CAShapeLayer;
     private var shapePath: UIBezierPath;
     
     init(size: CGSize, touchPoint: CGPoint) {
         self.firstPoint = touchPoint;
         self.lastPoint = self.firstPoint;
+        
+        self.points = [];
+        self.points.append(touchPoint);
+        
         self.shape = CAShapeLayer();
         self.shapePath = UIBezierPath();
         self.shapePath.move(to: CGPoint(x: 0, y: 0));
@@ -39,14 +44,25 @@ class SelectionLasso: UIView {
     
     public func addNewTouchPoint(touchPoint: CGPoint) -> Void {
         self.shape.removeFromSuperlayer();
+        self.points.append(touchPoint);
+        self.shapePath.removeAllPoints();
+        self.shapePath.move(to: CGPoint(x: 0, y: 0));
+        var circle: CAShapeLayer;
         
-        self.shapePath.addLine(to: CGPoint(
-            x: touchPoint.x - self.firstPoint.x,
-            y: touchPoint.y - self.firstPoint.y
-        ));
-        
+        for point in self.points {
+            self.shapePath.addLine(to: CGPoint(
+                x: point.x - self.firstPoint.x,
+                y: point.y - self.firstPoint.y
+            ));
+            
+            circle = CAShapeLayer();
+            circle.path = UIBezierPath(roundedRect: CGRect(x: -5, y: -5, width: 2.0 * self.radius, height: 2.0 * self.radius), cornerRadius: self.radius).cgPath;
+            circle.position = CGPoint(x: point.x, y: point.y);
+            circle.fillColor = UIColor.blue.cgColor;
+            self.layer.addSublayer(circle);
+        }
+        self.shapePath.close();
         self.shape.path = self.shapePath.cgPath;
-        
         self.layer.addSublayer(self.shape);
         
         self.lastPoint = touchPoint;
