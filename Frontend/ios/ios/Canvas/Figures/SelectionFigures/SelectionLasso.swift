b2e1dc00fdce0983df5f7be2139d401a0ad23fb0
Sebastien Cadorette <cado.94@me.com>
2019-03-26 14:18:51 -0400

    Simplification de code en déplacant du code dans des fonctions + suppression d'attributs inutiles.

--- Frontend/ios/ios/Canvas/Figures/SelectionFigures/SelectionLasso.swift
+++ Frontend/ios/ios/Canvas/Figures/SelectionFigures/SelectionLasso.swift
@@ -13,29 +13,26 @@ class SelectionLasso: UIView {
     private let radius: CGFloat = 5.0;
     
     private var firstPoint: CGPoint;
-    private var lastPoint: CGPoint;
     private var points: [CGPoint];
+    
     private var shape: CAShapeLayer;
     private var shapePath: UIBezierPath;
     
     init(size: CGSize, touchPoint: CGPoint) {
         self.firstPoint = touchPoint;
-        self.lastPoint = self.firstPoint;
         
         self.points = [];
         self.points.append(touchPoint);
         
         self.shape = CAShapeLayer();
         self.shapePath = UIBezierPath();
-        self.shapePath.move(to: CGPoint(x: 0, y: 0));
-        self.shape.fillColor = nil;
-        self.shape.lineWidth = 4;
-        self.shape.position = self.firstPoint;
-        self.shape.strokeColor = UIColor.black.cgColor;
-        self.shape.lineDashPattern = [4, 4];
         
         let frame: CGRect = CGRect(origin: CGPoint(x: 0, y: 0), size: size);
         super.init(frame: frame);
+        
+        self.setShapeProperties();
+        
+        self.addTouchPointCircle(touchPoint: touchPoint);
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -47,7 +44,6 @@ class SelectionLasso: UIView {
         self.points.append(touchPoint);
         self.shapePath.removeAllPoints();
         self.shapePath.move(to: CGPoint(x: 0, y: 0));
-        var circle: CAShapeLayer;
         
         for point in self.points {
             self.shapePath.addLine(to: CGPoint(
@@ -55,16 +51,27 @@ class SelectionLasso: UIView {
                 y: point.y - self.firstPoint.y
             ));
             
-            circle = CAShapeLayer();
-            circle.path = UIBezierPath(roundedRect: CGRect(x: -5, y: -5, width: 2.0 * self.radius, height: 2.0 * self.radius), cornerRadius: self.radius).cgPath;
-            circle.position = CGPoint(x: point.x, y: point.y);
-            circle.fillColor = UIColor.blue.cgColor;
-            self.layer.addSublayer(circle);
+            self.addTouchPointCircle(touchPoint: point);
         }
-        self.shapePath.close();
+        
         self.shape.path = self.shapePath.cgPath;
         self.layer.addSublayer(self.shape);
-        
-        self.lastPoint = touchPoint;
+    }
+    
+    private func setShapeProperties() -> Void {
+        self.shapePath.move(to: CGPoint(x: 0, y: 0));
+        self.shape.fillColor = nil;
+        self.shape.lineWidth = 4;
+        self.shape.position = self.firstPoint;
+        self.shape.strokeColor = UIColor.black.cgColor;
+        self.shape.lineDashPattern = [4, 4];
+    }
+    
+    private func addTouchPointCircle(touchPoint: CGPoint) -> Void {
+        let circle = CAShapeLayer();
+        circle.path = UIBezierPath(roundedRect: CGRect(x: -5, y: -5, width: 2.0 * self.radius, height: 2.0 * self.radius), cornerRadius: self.radius).cgPath;
+        circle.position = CGPoint(x: touchPoint.x, y: touchPoint.y);
+        circle.fillColor = UIColor.blue.cgColor;
+        self.layer.addSublayer(circle);
     }
 }
