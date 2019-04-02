d84178422bde520df3dd686f19aa172e2b6d48f1
chbinc <chelsy.binet@polymtl.ca>
2019-04-02 13:19:37 -0400

    db chelsy

--- Frontend/wpf/PolyPaint/Config.cs
+++ Frontend/wpf/PolyPaint/Config.cs
@@ -2,7 +2,7 @@
 {
     public static class Config
     {
-        public static string URL = "https://polypaint.me";
+        public static string URL = "https://localhost:44300";
         public static readonly int MIN_DISTANCE_ANCHORS = 10;
     }
 }
