fa7d2a284b0b1a9115f6c1f49f91dc835937a4bd
ollaua <olivier.lauzon@polymtl.ca>
2019-04-02 11:17:15 -0400

    polypaint.me

--- Frontend/wpf/PolyPaint/Config.cs
+++ Frontend/wpf/PolyPaint/Config.cs
@@ -2,7 +2,7 @@
 {
     public static class Config
     {
-        public static string URL = "http://192.168.1.13:3000";
+        public static string URL = "https://polypaint.me";
         public static readonly int MIN_DISTANCE_ANCHORS = 10;
     }
 }
