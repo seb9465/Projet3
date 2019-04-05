f377ae3b32a89e348b4f2e6a7343790593c2f92f
Sebastien Cadorette <cado.94@me.com>
2019-04-05 08:49:05 -0400

    typo

--- Frontend/ios/ios/Chat/New Group/ChatRoomsControllerTableViewController.swift
+++ Frontend/ios/ios/Chat/New Group/ChatRoomsControllerTableViewController.swift
@@ -126,6 +126,6 @@ class ChatRoomsControllerTableViewController: UITableViewController {
             self.tableView.reloadData();
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
 }
