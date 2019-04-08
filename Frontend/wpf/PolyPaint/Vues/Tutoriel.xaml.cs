5ced4e3b9933d40316e00b16fba4530d4bf04ced
chbinc <chelsy.binet@polymtl.ca>
2019-04-08 14:12:59 -0400

    tutorial has all images

--- Frontend/wpf/PolyPaint/Vues/Tutoriel.xaml.cs
+++ Frontend/wpf/PolyPaint/Vues/Tutoriel.xaml.cs
@@ -66,13 +66,13 @@ namespace PolyPaint.Vues
                     tutorialImage.Source = new BitmapImage(new Uri(@"/Resources/tutorial/2-0.png", UriKind.Relative));
                     tutorialText.Text = "By clicking this button, the chat will open in an external window ";
                     previousPageButton.Visibility = Visibility.Visible;
-                    nextPageButton.Visibility = Visibility.Collapsed;
+                    nextPageButton.Visibility = Visibility.Visible;
                     break;
                 case 4:
                     tutorialImage.Source = new BitmapImage(new Uri(@"/Resources/tutorial/2-1.png", UriKind.Relative));
                     tutorialText.Text = "In the gallery, yu can go to a canvas by selecting it in the list or create a new one by clicking on the plus button";
                     previousPageButton.Visibility = Visibility.Visible;
-                    nextPageButton.Visibility = Visibility.Collapsed;
+                    nextPageButton.Visibility = Visibility.Visible;
                     break;
                 case 5:
                     tutorialImage.Source = new BitmapImage(new Uri(@"/Resources/tutorial/2-2.png", UriKind.Relative));
@@ -80,37 +80,37 @@ namespace PolyPaint.Vues
                         " A public canvas can be seen by every user in the gallery. A private canvas can only be seen by yourself "
                         + "You will also need to select a protection. A protected canvas will require a password for other user to access it";
                     previousPageButton.Visibility = Visibility.Visible;
-                    nextPageButton.Visibility = Visibility.Collapsed;
+                    nextPageButton.Visibility = Visibility.Visible;
                     break;
                 case 6:
                     tutorialImage.Source = new BitmapImage(new Uri(@"/Resources/tutorial/3.png", UriKind.Relative));
                     tutorialText.Text = "Here is the canvas editor.";
                     previousPageButton.Visibility = Visibility.Visible;
-                    nextPageButton.Visibility = Visibility.Collapsed;
+                    nextPageButton.Visibility = Visibility.Visible;
                     break;
                 case 7:
                     tutorialImage.Source = new BitmapImage(new Uri(@"/Resources/tutorial/4.png", UriKind.Relative));
                     tutorialText.Text = "You can insert element to the canvas by selecting it in the menu";
                     previousPageButton.Visibility = Visibility.Visible;
-                    nextPageButton.Visibility = Visibility.Collapsed;
+                    nextPageButton.Visibility = Visibility.Visible;
                     break;
                 case 8:
                     tutorialImage.Source = new BitmapImage(new Uri(@"/Resources/tutorial/5.png", UriKind.Relative));
                     tutorialText.Text = "You can insert a connection figure to the canvas by selecting it in the menu";
                     previousPageButton.Visibility = Visibility.Visible;
-                    nextPageButton.Visibility = Visibility.Collapsed;
+                    nextPageButton.Visibility = Visibility.Visible;
                     break;
                 case 9:
                     tutorialImage.Source = new BitmapImage(new Uri(@"/Resources/tutorial/6.png", UriKind.Relative));
                     tutorialText.Text = "You can change the style of an element via the left side menu";
                     previousPageButton.Visibility = Visibility.Visible;
-                    nextPageButton.Visibility = Visibility.Collapsed;
+                    nextPageButton.Visibility = Visibility.Visible;
                     break;
                 case 10:
                     tutorialImage.Source = new BitmapImage(new Uri(@"/Resources/tutorial/7.png", UriKind.Relative));
                     tutorialText.Text = "You can rotate an element via the left side menu";
                     previousPageButton.Visibility = Visibility.Visible;
-                    nextPageButton.Visibility = Visibility.Collapsed;
+                    nextPageButton.Visibility = Visibility.Visible;
                     break;
                 case 11:
                     tutorialImage.Source = new BitmapImage(new Uri(@"/Resources/tutorial/8.png", UriKind.Relative));
