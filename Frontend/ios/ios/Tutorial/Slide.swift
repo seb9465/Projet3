5717749c4401e73dfaac2f49cdf30e29b23b2891
Sebastien Cadorette <cado.94@me.com>
2019-04-08 08:49:32 -0400

    Ajout d'un bouton qui sera afficher à la fin du tutoriel pour aller au dashboard

--- Frontend/ios/ios/Tutorial/Slide.swift
+++ Frontend/ios/ios/Tutorial/Slide.swift
@@ -17,12 +17,36 @@ class Slide: UIView {
     @IBOutlet var imageView: UIImageView!;
     @IBOutlet var labelTitle: UILabel!;
     @IBOutlet var labelDesc: UILabel!;
+    @IBOutlet var getStartedButon: UIButton!
+    
+    // MARK: - Constructors
+    
+    override init(frame: CGRect) {
+        self.labelTitle.textColor = Constants.RED_COLOR;
+        self.labelDesc.textColor = Constants.RED_COLOR;
+        
+        
+        
+        super.init(frame: frame);
+        
+        self.initGetStartedButton();
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Timing functions
     
     // MARK: - Public functions
     
     // MARK: - Private functions
+    
+    private func initGetStartedButton() -> Void {
+        self.getStartedButon.tintColor = UIColor.white.cgColor;
+        self.getStartedButon.layer.backgroundColor = Constants.RED_COLOR;
+        
+    }
     
     /*
     // Only override draw() if you perform custom drawing.
