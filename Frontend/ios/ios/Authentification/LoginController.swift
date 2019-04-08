8e2073cee233934772d98121cd13b3026fe2c377
William Sevigny <wsvgny@gmail.com>
2019-04-08 17:52:43 -0400

    UI - leger

--- Frontend/ios/ios/Authentification/LoginController.swift
+++ Frontend/ios/ios/Authentification/LoginController.swift
@@ -52,8 +52,8 @@ class LoginController: UIViewController, UITextFieldDelegate, WKUIDelegate {
         let spinner = UIViewController.displaySpinner(onView: self.view);
         
 //        AuthentificationAPI.login(username: emailField.text!, password: passwordField.text!)
-//        AuthentificationAPI.login(username: "william.sevigny", password: "!12345Aa")
-        AuthentificationAPI.login(username: "seb.cado2", password: "!12345Aa")
+        AuthentificationAPI.login(username: "william.sevigny", password: "!12345Aa")
+//        AuthentificationAPI.login(username: "seb.cado2", password: "!12345Aa")
             .done { (token) in
                 UIViewController.removeSpinner(spinner: spinner);
                 self.validationLabel.text = "";
