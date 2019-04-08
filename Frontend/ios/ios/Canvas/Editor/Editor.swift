27b8a3b26a15710808527018476daea4f29634f5
William Sevigny <wsvgny@gmail.com>
2019-04-08 19:07:01 -0400

    re added double tap

--- Frontend/ios/ios/Canvas/Editor/Editor.swift
+++ Frontend/ios/ios/Canvas/Editor/Editor.swift
@@ -48,9 +48,9 @@ class Editor {
         self.editorView.delegate = self
         let pinch = UIPinchGestureRecognizer(target: self, action: #selector(self.resizeFigure(_:)))
         self.editorView.addGestureRecognizer(pinch)
-//        let tap = UITapGestureRecognizer(target: self, action: #selector(self.tap(_:)))
-//        tap.numberOfTapsRequired = 2
-//        self.editorView.addGestureRecognizer(tap)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.tap(_:)))
+        tap.numberOfTapsRequired = 2
+        self.editorView.addGestureRecognizer(tap)
     }
     func resize(width: CGFloat, heigth: CGFloat) {
         currentCanvas.canvasHeight = Float(heigth)
