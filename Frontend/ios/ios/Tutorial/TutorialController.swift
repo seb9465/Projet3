ed65850545c53d4982bd866b8645b221f57bc9f6
Sebastien Cadorette <cado.94@me.com>
2019-04-07 23:51:59 -0400

    Ajout du contrôleur pour le tutoriel

---
+++ Frontend/ios/ios/Tutorial/TutorialController.swift
@@ -0,0 +1,19 @@
+//
+//  TutorialController.swift
+//  ios
+//
+//  Created by Sebastien Cadorette on 2019-04-07.
+//  Copyright © 2019 LOG3000 equipe 12. All rights reserved.
+//
+
+import UIKit
+
+class TutorialController: UIViewController {
+    
+    // MARK: - Attributes
+    
+    // MARK: - Outlets
+    
+    @IBOutlet var scrollView: UIScrollView!
+    
+}
