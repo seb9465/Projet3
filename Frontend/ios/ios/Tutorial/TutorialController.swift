258cf0a2124b116be32644dfaf26e5121138f29e
Sebastien Cadorette <cado.94@me.com>
2019-04-08 00:57:40 -0400

    Ajout fonction pour générer les slides & initialiser le scroll view

--- Frontend/ios/ios/Tutorial/TutorialController.swift
+++ Frontend/ios/ios/Tutorial/TutorialController.swift
@@ -12,9 +12,40 @@ class TutorialController: UIViewController {
     
     // MARK: - Attributes
     
-    // MARK: - Outlets
     
+    // MARK: - Outlets
+
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var pageControle: UIPageControl!
     
+    // MARK: - Timing functions
+    
+    override func viewDidLoad() {
+        let slides: [Slide] = self.createSlides();
+    }
+    
+    // MARK: - Public functions
+    
+    
+    // MARK: - Private functions
+    
+    private func createSlides() -> [Slide] {
+        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide;
+        slide1.imageView.image = UIImage(named: "ic_onboarding_1");
+        slide1.labelTitle.text = "A real-life bear";
+        slide1.labelDesc.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London";
+        
+        return [slide1];
+    }
+    
+    private func setupSlideScrollView(slides: [Slide]) -> Void {
+        self.scrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height);
+        self.scrollView.contentSize = CGSize(width: self.view.frame.width * CGFloat(slides.count), height: self.view.frame.height);
+        self.scrollView.isPagingEnabled = true;
+        
+        for slide in slides {
+            slide.frame = CGRect(x: self.view.frame.width * CGFloat(i), y: 0, width: self.view.frame.width, height: self.view.frame.height)
+            self.scrollView.addSubview(slide)
+        }
+    }
 }
