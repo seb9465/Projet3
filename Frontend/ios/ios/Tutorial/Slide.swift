4bd0018a618aa373a97565bfc68e3b3c263b2a8d
Sebastien Cadorette <cado.94@me.com>
2019-04-07 23:58:03 -0400

    Ajout d'un fichier controleur pour le Slide

---
+++ Frontend/ios/ios/Tutorial/Slide.swift
@@ -0,0 +1,31 @@
+//
+//  Slide.swift
+//  ios
+//
+//  Created by Sebastien Cadorette on 2019-04-07.
+//  Copyright © 2019 LOG3000 equipe 12. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    // MARK: - Attributes
+    
+    // MARK: - Outlets
+    
+    // MARK: - Timing functions
+    
+    // MARK: - Public functions
+    
+    // MARK: - Private functions
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
